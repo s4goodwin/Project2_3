dafcfb06caac75aaa0acaf4fb81b0e9a302dc419
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-10-22 18:37:47 -0400

    updated the order class, added cartForOrder arraylist and cartForOrder=orderCart

--- src/Order.java
+++ src/Order.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+
 public class Order {
     private ShippingAddress destination;
     private Customer orderedBy;
+    private ArrayList<merchandiseItem> cartForOrder;
+    
 
-    public Order(ShippingAddress dest, Customer cust) {
+    public Order(ShippingAddress dest, Customer cust,ArrayList<merchandiseItem> orderCart) {
         destination=dest;
         orderedBy=cust;
+        cartForOrder=orderCart;
     }
     public String getDestination(){
         return destination.toString();
---
+++ src/merchandiseItem.java
@@ -0,0 +1,2 @@
+public class merchandiseItem {
+}
