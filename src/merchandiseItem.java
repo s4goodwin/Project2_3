399594a3881683388eeaf153ffd895fcd7d60e97
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-10-22 19:25:13 -0400

    merchendice class
    added 3 instance variables, 3 get accessors, constructor constructed

--- src/merchandiseItem.java
+++ src/merchandiseItem.java
@@ -1,2 +1,20 @@
 public class merchandiseItem {
+    private String Name;
+    private double price;
+    private ItemType taxibleItem;
+
+    public merchandiseItem(String Name,double price, ItemType type){
+        this.Name=Name;
+        this.price=price;
+        taxibleItem=type;
+    }
+    public String getName(){
+        return Name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public ItemType getType() {
+        return taxibleItem;
+    }
 }
